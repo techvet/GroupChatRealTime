8410a462e074782f554951a11c8af18f89c56b50
gc <vetsfortech@gmail.com>
2014-06-24 23:27:58 -0500

    Started UI and Index

--- GroupChat/ChatHub.cs
+++ GroupChat/ChatHub.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 
 namespace GroupChat
 {
-    [HubName("chat")]
-    public class ChatHub
+    [HubName("chatHub")]
+    public class ChatHub : Hub
     {
-
+        public void Send(string name, string message)
+        {
+            Clients.All.broadCastMessage(name, message);
+        }
     }
 }
