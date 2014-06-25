dbfeff00649fc3539d52f8ec48623665d47b612f
gc <vetsfortech@gmail.com>
2014-06-25 00:35:07 -0500

    Group chat implementation. Groups can now talk to each other and users can freely jump from room to room.

--- GroupChat/ChatHub.cs
+++ GroupChat/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
@@ -10,9 +11,20 @@ namespace GroupChat
     [HubName("chatHub")]
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public async Task JoinRoom(string userName, string roomName)
         {
-            Clients.All.broadCastMessage(name, message);
+            await Groups.Add(Context.ConnectionId, roomName);
+            Clients.Group(roomName).broadCastMessage(userName + ": joined room: " + roomName);
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+
+        public void Send(string name, string message, string groupName)
+        {
+            Clients.Group(groupName).broadCastMessage(name, message);
         }
     }
 }
